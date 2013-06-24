73ef67395315d95fc055cefaae6fa84f1ab099ca
Tunbosun Ayinla <tayinla@gmail.com>
2013-06-24 07:21:39 +0100

    Initial Footer
    Display the blog name and the current year in the footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+	<div class="row">
+		<div class="twelve columns">
+			<footer class="footer" role="contentinfo">
+				<div id="inner-footer" class="wrap clearfix">
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+				</div> <!-- end #inner-footer -->
+			</footer> <!-- end footer -->
+		</div>
+	</div>
+
+</div> <!-- end #container -->
+	<?php wp_footer(); ?>
+	</body>
+</html> <!-- end page. what a ride! -->
